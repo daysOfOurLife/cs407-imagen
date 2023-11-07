07bdbd5fb0d80ff8d1482d1e4fd843fe1e07d727
Victor Nguyen <91512538+daysOfOurLife@users.noreply.github.com>
2023-11-07 09:43:49 -0600

    Initial Commit

---
+++ Imagen/ImagenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagenApp.swift
+//  Imagen
+//
+//  Created by Victor Nguyen on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ImagenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
